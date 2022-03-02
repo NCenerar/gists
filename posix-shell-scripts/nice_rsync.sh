904b338ef85dc38702a1abd82ad376ecab8f94dc
Nicolas Cenerario <nicolas.cenerario@outlook.fr>
2022-03-02 16:47:30 +0100

    fix variable format consistency and use brackets

--- posix-shell-scripts/nice_rsync.sh
+++ posix-shell-scripts/nice_rsync.sh
@@ -2,16 +2,16 @@
 set -e
 
 NBRETRY=9
-while [ $# -gt 0 ] ; do
-  case "$1" in
-    --nb-retry) NBRETRY=$2 ; shift 2 ;;
+while [ ${#} -gt 0 ] ; do
+  case "${1}" in
+    --nb-retry) NBRETRY=${2} ; shift 2 ;;
     --) shift ; break ;;
     *)          break ;;
   esac
 done
 
 if [ ${#} -lt 2 ] ; then
-  printf "$0 [--nb-retry n] [--] [rsync options] SRC DST
+  printf "${0} [--nb-retry n] [--] [rsync options] SRC DST
     --nb-retry  n   Number of retry in case of failure. (default to 9)
 
   rsync options are system specific.
--- posix-shell-scripts/nice_runner.sh
+++ posix-shell-scripts/nice_runner.sh
@@ -4,18 +4,18 @@ set -e
 BRANCH="main"
 FOLDER="posix-shell-scripts"
 SCRIPT="nice_install.sh"
-while [ $# -gt 0 ] ; do
-  case "$1" in
-    --branch) BRANCH="$2" ; shift 2 ;;
-    --folder) FOLDER="$2" ; shift 2 ;;
-    --script) SCRIPT="$2" ; shift 2 ;;
+while [ ${#} -gt 0 ] ; do
+  case "${1}" in
+    --branch) BRANCH="${2}" ; shift 2 ;;
+    --folder) FOLDER="${2}" ; shift 2 ;;
+    --script) SCRIPT="${2}" ; shift 2 ;;
     --) shift ; break ;;
     *)          break ;;
   esac
 done
 
 if [ ${#} -gt 0 ] ; then
-  printf "$0 [--branch name] [--folder name] [--script name] [--]
+  printf "${0} [--branch name] [--folder name] [--script name] [--]
   --branch  name  Name of the branch to use. (default to main)
   --folder  name  Name of the folder to use. (default to posix-shell-scripts)
   --script  name  Name of the script to use. (default to nice_install.sh)
